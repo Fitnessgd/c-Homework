ee8c88d4daa0b34820e7a012804bdcea3231a4fd
Fitnessgd <62288511+Fitnessgd@users.noreply.github.com>
2020-04-04 14:52:23 +0300

    Add files via upload

---
+++ Homework.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+                static void Main(string[] args)
+        {
+
+
+            // ----------matrix sring : --------------
+           
+            string[][] matrix1 = new string[3][]
+            {
+                new string[]{"main", "up", "sub"},
+                new string[]{"down", "center" , "up"},
+                new string[]{"sub" , "down" , "main"}
+            };
+
+
+            Console.WriteLine("This is a matirx: ");
+            foreach (string[] Row in matrix1)
+            {
+                foreach (string Col in Row)
+                {
+                    Console.Write(Col + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            /*
+             * This is a jagged array:
+                main up sub
+                down center up
+                sub down main
+            */
+
+            Console.ReadKey();
+
+        }
+    }
+}
